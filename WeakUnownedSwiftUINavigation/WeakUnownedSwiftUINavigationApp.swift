1e1eb9fe4cc93422ded8437447da0a231be30acd
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-08-02 01:41:01 +0700

    Initial Commit

---
+++ WeakUnownedSwiftUINavigation/WeakUnownedSwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeakUnownedSwiftUINavigationApp.swift
+//  WeakUnownedSwiftUINavigation
+//
+//  Created by Jason Rich Darmawan Onggo Putra on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeakUnownedSwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
